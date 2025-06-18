8a0cc50b1def5e1433fdc177ec4f43e60e9f8716
aissacordero <aissacordero605@gmail.com>
2025-06-18 10:50:28 -0700

    Create solitaire.js

---
+++ solitaire.js
@@ -0,0 +1,6 @@
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
+
+ctx.font = '30px Arial';
+ctx.fillStyle = 'white';
+ctx.fillText('Solitaire Game Coming Soon...', 200, 300);
